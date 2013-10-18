b0f783174787625470164aab5b2dbaf9b971681d
Navedonar <97.sebastian@gmail.com>
2013-10-18 12:59:36 +0200

    Fix failed quests

---
+++ sql/updates/world/failed_quest_fix.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `Health_mod`=1 WHERE `entry` IN (2031, 1984, 1988, 2032, 1986, 2079, 2077, 8583, 1985, 1989, 49874, 42937, 50039, 197);
+UPDATE `creature_template` SET `Health_mod`=2.5 WHERE `entry` =1994;
+UPDATE `creature_template` SET `Mana_mod`=1 WHERE `entry` IN (43006, 43305);
+UPDATE `quest_template` SET `LimitTime` = '0' WHERE `entry` IN (9640, 8801, 11503, 11502, 25924, 8784, 7161, 7162, 10612, 10613, 14482, 13385, 13384, 8114, 8115, 8121, 8122, 8105, 8120, 10946);
